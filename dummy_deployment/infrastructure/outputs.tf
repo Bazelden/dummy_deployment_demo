f40ff8aff127bd7a2512abece8fef638880cdcfa
Bazelden <jack.waller@hotmail.com>
2024-07-15 16:17:53 +0100

    Altered ip output

--- dummy_deployment/infrastructure/outputs.tf
+++ dummy_deployment/infrastructure/outputs.tf
@@ -1,5 +1,6 @@
 #outputs from our terraform configuration
 
 output "instance_ip" {
-  value = aws_instance.dummy_host_instance.public_ip
+  value       = aws_instance.dummy_host_instance.public_ip
+  description = "value of the public ip of the ec2 instance"
 }
