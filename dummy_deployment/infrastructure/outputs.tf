265abc3e5d1d729413776bf3e1bf136397283a1f
Bazelden <jack.waller@hotmail.com>
2024-07-15 16:50:20 +0100

    Attempting to debug IP issue

--- dummy_deployment/infrastructure/outputs.tf
+++ dummy_deployment/infrastructure/outputs.tf
@@ -2,5 +2,5 @@
 
 output "instance_ip" {
   value       = aws_instance.dummy_host_instance.public_ip
-  description = "value of the public ip of the ec2 instance"
+  description = "The public IP address of the EC2 instance"
 }
